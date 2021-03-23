0e24a8f786616959eb21072d2efa22f7421980eb
Luis Palma <palmaluis1997@gmail.com>
2021-03-23 19:26:47 -0400

    branch office

---
+++ database/factories/BranchOfficeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\BranchOffice;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BranchOfficeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = BranchOffice::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/seeders/BranchOfficeSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+class BranchOfficeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('branch_offices')->insert([
+            [
+                'name' => 'Sucursal 1',
+                'description' => 'Sucursal 1',
+                'user_created_id' => 1
+            ],
+            [
+                'name' => 'Sucursal 2',
+                'description' => 'Sucursal 2',
+                'user_created_id' => 1
+            ]
+        ]);
+    }
+}
