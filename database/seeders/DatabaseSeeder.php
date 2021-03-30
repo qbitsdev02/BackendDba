bce268ec48cd2338915a2257b1edef9ca1b85325
Luis Palma <palmaluis1997@gmail.com>
2021-03-30 00:28:56 -0400

    products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,15 +16,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(4)->create();
-        \App\Models\Client::factory(10)->create();
         $this->call([
             RoleSeeder::class,
             RoleUserSeeder::class,
+            ClientTypeSeeder::class,
+            DocumentTypeSeeder::class
+        ]);
+        \App\Models\Client::factory(10)->create();
+        $this->call([
             BrandSeeder::class,
             CategorySeeder::class,
             AttributeTypeSeeder::class,
-            ClientTypeSeeder::class,
-            DocumentTypeSeeder::class,
             CoinSeeder::class,
             OperationTypeSeeder::class,
             VoucherTypeSeeder::class,
