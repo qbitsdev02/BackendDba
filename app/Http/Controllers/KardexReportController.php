9deff845bf44171349a65139e5d5d939936c341e
Luis Palma <palmaluis1997@gmail.com>
2021-09-14 16:01:21 -0400

    order

--- app/Http/Controllers/KardexReportController.php
+++ app/Http/Controllers/KardexReportController.php
@@ -106,17 +106,17 @@ class KardexReportController extends Controller
             ->with('product:id,description,name')
             ->where('product_id', $request->product_id)
             ->betweenDate($request->all())
-            ->orderBy('created_at', 'desc')
             ->get();
 
         $purchaseDetails = PurchaseDetail::select('product_id', 'created_at', 'amount', 'purchase_id', 'stock')
             ->with('product:id,description,name')
             ->where('product_id', $request->product_id)
             ->betweenDate($request->all())
-            ->orderBy('created_at', 'desc')
             ->get();
 
-        return response()->json([...$billDetails, ...$purchaseDetails], 200);
+        $all = collect([...$billDetails, ...$purchaseDetails])->sortBy([['created_at', 'desc']]);
+
+        return response()->json($all, 200);
     }
 
     /**
