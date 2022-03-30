806fc9891bbf7be271483dbaaecb7e1704c41903
Luis Palma <palmaluis1997@gmail.com>
2022-03-30 14:30:36 -0400

    operation type

--- app/Models/OperationType.php
+++ app/Models/OperationType.php
@@ -2,10 +2,39 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class OperationType extends Model
+/**
+ * @OA\Schema(
+ *   schema="OperationType",
+ *   type="object",
+ *   @OA\Property(
+ *       property="name",
+ *       type="string",
+ *       required={"true"},
+ *       description="The OperationType name"
+ *   ),
+ *   @OA\Property(
+ *       property="description",
+ *       type="string",
+ *       required={"true"},
+ *       description="The OperationType description"
+ *   ),
+ *   @OA\Property(
+ *       property="user_created_id",
+ *       type="number",
+ *       required={"true"},
+ *       example=1,
+ *       description="The Users crete"
+ *   ),
+ *    @OA\Property(
+ *       property="user_updated_id",
+ *       type="number",
+ *       required={"true"},
+ *       example=1,
+ *       description="The Users update"
+ *   ),
+ * )
+ */
+class OperationType extends Base
 {
-    use HasFactory;
+    //
 }
