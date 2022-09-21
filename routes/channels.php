839bb39f7217e82147b08e321cdf5d9470bbd956
Luis Palma <palmaluis1997@gmail.com>
2022-09-20 21:16:19 -0400

    bug

--- routes/channels.php
+++ routes/channels.php
@@ -17,9 +17,9 @@ use Illuminate\Support\Facades\Broadcast;
 //     return (int) $user->id === (int) $id;
 // });
 
-    Broadcast::channel('transaction', function ($user) {
-        return true;
-    });
+    // Broadcast::channel('transaction', function ($user) {
+    //     return true;
+    // });
 
 
 // Broadcast::channel('order.{orderId}', function ($user, $orderId) {
