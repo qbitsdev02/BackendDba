a0d37f1b84127a1b69118a9c91e89736df5fea86
palma001 <palmaluis1997@gmail.com>
2023-05-17 00:53:35 -0400

    change

--- database/seeders/ChartOfAccountSeeder.php
+++ database/seeders/ChartOfAccountSeeder.php
@@ -8,6 +8,30 @@ use Illuminate\Database\Seeder;
 
 class ChartOfAccountSeeder extends Seeder
 {
+        /**
+     * Recursive function to create concepts with their children.
+     *
+     * @param  array  $concepts
+     * @param  int|null  $parentConceptId
+     * @param  string|null  $parentCode
+     * @return void
+     */
+    private function createConcepts(array $concepts, ?int $parentConceptId, ?string $parentCode = null)
+    {
+        foreach ($concepts as $index => $conceptData) {
+            $code = $parentCode ? $parentCode . '.' . ($index + 1) : ($index + 1);
+
+            $concept = new ChartOfAccount();
+            $concept->name = $conceptData['name'];
+            $concept->concept_id = $parentConceptId;
+            $concept->code = $code;
+            $concept->save();
+
+            if (isset($conceptData['children']) && is_array($conceptData['children'])) {
+                $this->createConcepts($conceptData['children'], $concept->id, $code);
+            }
+        }
+    }
     /**
      * Run the database seeds.
      *
@@ -15,32 +39,42 @@ class ChartOfAccountSeeder extends Seeder
      */
     public function run()
     {
-        $accounts = [
-            ['name' => 'Activos', 'code' => '1', 'chart_of_account_id' => null, 'user_created_id' => 1],
-            ['name' => 'Activos corrientes', 'code' => '1.1', 'chart_of_account_id' => 1, 'user_created_id' => 1],
-            ['name' => 'Caja', 'code' => '1.1.1', 'chart_of_account_id' => 2, 'user_created_id' => 1],
-            ['name' => 'Bancos', 'code' => '1.1.2', 'chart_of_account_id' => 2, 'user_created_id' => 1],
-            ['name' => 'Inversiones', 'code' => '1.1.3', 'chart_of_account_id' => 2, 'user_created_id' => 1],
-            ['name' => 'Activos no corrientes', 'code' => '1.2', 'chart_of_account_id' => 1, 'user_created_id' => 1],
-            ['name' => 'Propiedades, planta y equipo', 'code' => '1.2.1', 'chart_of_account_id' => 6, 'user_created_id' => 1],
-            ['name' => 'Terrenos', 'code' => '1.2.1.1', 'chart_of_account_id' => 7, 'user_created_id' => 1],
-            ['name' => 'Edificios', 'code' => '1.2.1.2', 'chart_of_account_id' => 7, 'user_created_id' => 1],
-            ['name' => 'Maquinaria', 'code' => '1.2.1.3', 'chart_of_account_id' => 7, 'user_created_id' => 1],
-            ['name' => 'Depreciación acumulada', 'code' => '1.2.2', 'chart_of_account_id' => 6, 'user_created_id' => 1],
-            ['name' => 'Pasivos', 'code' => '2', 'chart_of_account_id' => null, 'user_created_id' => 1],
-            ['name' => 'Pasivos corrientes', 'code' => '2.1', 'chart_of_account_id' => 12, 'user_created_id' => 1],
-            ['name' => 'Cuentas por pagar', 'code' => '2.1.1', 'chart_of_account_id' => 13, 'user_created_id' => 1],
-            ['name' => 'Sueldos por pagar', 'code' => '2.1.2', 'chart_of_account_id' => 13, 'user_created_id' => 1],
-            ['name' => 'Impuestos por pagar', 'code' => '2.1.3', 'chart_of_account_id' => 13, 'user_created_id' => 1],
-            ['name' => 'Pasivos no corrientes', 'code' => '2.2', 'chart_of_account_id' => 12, 'user_created_id' => 1],
-            ['name' => 'Préstamos a largo plazo', 'code' => '2.2.1', 'chart_of_account_id' => 16, 'user_created_id' => 1],
-            ['name' => 'Patrimonio', 'code' => '3', 'chart_of_account_id' => null, 'user_created_id' => 1],
-            ['name' => 'Capital social', 'code' => '3.1', 'chart_of_account_id' => 18, 'user_created_id' => 1],
-            ['name' => 'Utilidades retenidas', 'code' => '3.2', 'chart_of_account_id' => 18, 'user_created_id' => 1],
-        ];
+        $jsonFile = file_get_contents(app_path('Data/tree.json'));
+        $conceptType = json_decode($jsonFile);
 
-        foreach ($accounts as $account) {
-            ChartOfAccount::create($account);
+        // $conceptType = collect($conceptType)
+        //     ->where('TipodePartida', null)
+        //     ->where('Concepto', null);
+
+        $conceptType = collect($conceptType)->keyBy('');
+
+        foreach($conceptType as $egressType)
+        {
+
+
+            // $this->save($egressType->TipodePartida, $egressType->Code);
+
+            // if (isset($egressType->Concepto) && $egressType->Concepto) {
+            //     $this->save($egressType->Concepto, $egressType->Code, $egressType->TipodePartida);
+            // }
+
+            if (isset($egressType->TipodePartida) && $egressType->TipodePartida) {
+                $this->save($egressType->TipodePartida, $egressType->Code, $egressType->Partida);
+            }
+        }
+    }
+
+    static function save($name, $code, $findName = null) {
+        $chartOfAccountFind = ChartOfAccount::where('name', $name)->first();
+        $chartOfAccountId = ChartOfAccount::where('name', $findName)->first();
+
+        if (!$chartOfAccountFind) {
+            $chartOfAccount = new ChartOfAccount();
+            $chartOfAccount->name = $name;
+            $chartOfAccount->code = $code;
+            $chartOfAccount->chart_of_account_id = $chartOfAccountId ? $chartOfAccountId->id : null;
+            $chartOfAccount->user_created_id = 1;
+            $chartOfAccount->save();
         }
     }
 }
